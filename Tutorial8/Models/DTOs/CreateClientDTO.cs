1a4a1ac470a76d32120830f41258adb3a7210624
Julian Pulawski <s31873@pjwstk.edu.pl>
2025-05-05 23:57:03 +0200

    done

---
+++ Tutorial8/Models/DTOs/CreateClientDTO.cs
@@ -0,0 +1,8 @@
+﻿public class CreateClientDTO
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Telephone { get; set; } = null!;
+    public string Pesel { get; set; } = null!;
+}
---
+++ Tutorial8/Services/ITripsService.cs
@@ -0,0 +1,17 @@
+﻿using Tutorial8.Models.DTOs;
+
+namespace Tutorial8.Services;
+
+public interface ITripsService
+{
+    Task<List<TripDTO>> GetTrips();
+
+    Task<List<ClientTripDTO>?> GetClientTrips(int clientId);
+    Task<int?> CreateClientAsync(CreateClientDTO clientDto);
+
+    Task<string?> RegisterClientToTripAsync(int clientId, int tripId);
+    
+    Task<string?> UnregisterClientFromTripAsync(int clientId, int tripId);
+
+    
+}
